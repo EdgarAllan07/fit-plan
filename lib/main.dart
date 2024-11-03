ef17b0b739cce26d90d2fb814f4c4872b39dc320
Allan <108568433+EdgarAllan07@users.noreply.github.com>
2024-11-03 09:28:13 -0600

    Conectando con firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'paginas/Notas/CrearNotaScreen.dart';
-import 'paginas/menu.dart';
+import 'paginas/login.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,11 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+     debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home:Menu(),
+      home:Login(),
     );
   }
 }
