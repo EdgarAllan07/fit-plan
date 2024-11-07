0f444a1c95d2b40877b375e2aa1d052411504e1a
kevinasencio18 <asenciok70@gmail.com>
2024-11-07 11:59:22 -0600

    FUncionalidad de agregar Notas y otras acciones, estos son los aportes de Angel, pero ha tenido problemas en subir cambios!!!

--- lib/paginas/Notas/ListaNotasScreen.dart
+++ lib/paginas/Notas/ListaNotasScreen.dart
@@ -1,75 +1,170 @@
-import 'package:fit_plan_proyecto/paginas/Notas/CrearNotaScreen.dart';
 import 'package:flutter/material.dart';
-import 'package:fit_plan_proyecto/paginas/Notas/nota.dart';
+import 'package:fit_plan_proyecto/paginas/Notas/CrearNotaScreen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:intl/intl.dart'; // Importa intl para formatear la fecha
 
-class ListaNotasScreen extends StatefulWidget {
-  @override
-  _ListaNotasScreenState createState() => _ListaNotasScreenState();
-}
-
-class _ListaNotasScreenState extends State<ListaNotasScreen> {
-  List<Nota> notas = []; // Lista de objetos Nota
-
+class ListaNotasScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    User? usuarioActual = FirebaseAuth.instance.currentUser;
+
+    if (usuarioActual == null) {
+      return Scaffold(
+        appBar: AppBar(title: const Text("Mis Notas")),
+        body:
+            const Center(child: Text("No se encontró un usuario autenticado")),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Mis Notas'),
-        backgroundColor: Color(0xffffa07a),
+        title: const Text('Mis Notas'),
+        backgroundColor: const Color(0xffffa07a),
       ),
-      body: ListView.builder(
-        itemCount: notas.length,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 252, 2, 2),
-                border: Border.all(
-                  color: const Color.fromARGB(255, 235, 114, 8),
-                  width: 1.5,
-                ),
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              padding: EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    notas[index].titulo,
-                    style: TextStyle(
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.bold,
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('usuarios')
+            .doc(usuarioActual.uid)
+            .collection('notas')
+            .orderBy('fecha', descending: true)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+            return const Center(child: Text('No hay notas disponibles.'));
+          }
+
+          final notas = snapshot.data!.docs;
+
+          return ListView.builder(
+            itemCount: notas.length,
+            itemBuilder: (context, index) {
+              final nota = notas[index];
+              final String titulo = nota['titulo'] ?? 'Sin título';
+              final String contenido = nota['contenido'] ?? 'Sin contenido';
+
+              // Obtener y formatear la fecha de creación
+              final Timestamp? fechaTimestamp = nota['fecha'] as Timestamp?;
+              final String fechaFormateada = fechaTimestamp != null
+                  ? DateFormat('dd/MM/yyyy HH:mm')
+                      .format(fechaTimestamp.toDate())
+                  : 'Fecha no disponible';
+
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 244, 241, 240),
+                    border: Border.all(
+                      color: const Color.fromARGB(255, 235, 114, 8),
+                      width: 1.5,
                     ),
+                    borderRadius: BorderRadius.circular(10.0),
                   ),
-                  SizedBox(height: 8.0),
-                  Text(
-                    notas[index].contenido,
-                    style: TextStyle(fontSize: 16.0),
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        titulo,
+                        style: const TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 8.0),
+                      Text(
+                        contenido,
+                        style: const TextStyle(fontSize: 16.0),
+                      ),
+                      const SizedBox(height: 8.0),
+                      Text(
+                        'Fecha de creación: $fechaFormateada',
+                        style: TextStyle(
+                          fontSize: 14.0,
+                          color: Colors.grey[600],
+                        ),
+                      ),
+                      const SizedBox(height: 8.0),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: IconButton(
+                          icon: const Icon(Icons.delete, color: Colors.red),
+                          onPressed: () {
+                            _confirmarEliminacion(
+                                context, usuarioActual.uid, nota.id);
+                          },
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ),
+                ),
+              );
+            },
           );
         },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          // Navegar a la pantalla de creación de notas
           Navigator.push(
             context,
             MaterialPageRoute(
-                builder: (context) => CrearNotaScreen(onSave: (Nota nuevaNota) {
-                      setState(() {
-                        notas.add(nuevaNota);
-                      });
-                    })),
+              builder: (context) => const CrearNotaScreen(),
+            ),
           );
         },
-        child: Icon(Icons.add),
         tooltip: 'Agregar Nota',
-     backgroundColor: Color(0xffffa07a),
+        backgroundColor: const Color(0xffffa07a),
+        child: const Icon(Icons.add),
       ),
+    );
+  }
+
+  void _confirmarEliminacion(
+      BuildContext context, String userId, String notaId) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Confirmar eliminación"),
+          content:
+              const Text("¿Estás seguro de que deseas eliminar esta nota?"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text("Cancelar"),
+              style: TextButton.styleFrom(
+                foregroundColor: const Color(0xffffa07a), // Color del texto
+              ),
+            ),
+            TextButton(
+              onPressed: () async {
+                await FirebaseFirestore.instance
+                    .collection('usuarios')
+                    .doc(userId)
+                    .collection('notas')
+                    .doc(notaId)
+                    .delete();
+
+                Navigator.of(context).pop();
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text("Nota eliminada con éxito",
+                        style: TextStyle(color: Colors.white)),
+                    backgroundColor: Color(0xffffa07a),
+                  ),
+                );
+              },
+              child: const Text("Eliminar"),
+            ),
+          ],
+        );
+      },
     );
   }
 }
--- lib/paginas/calcularPeso/resultadoPeso.dart
+++ lib/paginas/calcularPeso/resultadoPeso.dart
@@ -1,8 +1,102 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class ResultadoPeso  extends StatelessWidget {
+class ResultadoPeso extends StatefulWidget {
+  @override
+  _ResultadoPesoState createState() => _ResultadoPesoState();
+}
 
-    @override
+class _ResultadoPesoState extends State<ResultadoPeso> {
+  final TextEditingController _pesoActualController = TextEditingController();
+  final TextEditingController _pesoInicialController = TextEditingController();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  double? pesoInicial;
+  DateTime? fechaPesoInicial;
+  String _resultado = '';
+  bool _editandoPesoInicial = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _cargarPesoInicial();
+  }
+
+  Future<void> _cargarPesoInicial() async {
+    User? user = _auth.currentUser;
+    if (user != null) {
+      DocumentSnapshot usuarioDoc = await FirebaseFirestore.instance
+          .collection('usuarios')
+          .doc(user.uid)
+          .get();
+
+      if (usuarioDoc.exists) {
+        setState(() {
+          pesoInicial = usuarioDoc['pesoInicial'];
+          fechaPesoInicial =
+              (usuarioDoc['fechaPesoInicial'] as Timestamp).toDate();
+          _pesoInicialController.text = pesoInicial!.toStringAsFixed(1);
+        });
+      }
+    }
+  }
+
+  Future<void> _guardarPesoInicial(double peso) async {
+    User? user = _auth.currentUser;
+    if (user != null) {
+      DateTime fechaActual = DateTime.now();
+      await FirebaseFirestore.instance
+          .collection('usuarios')
+          .doc(user.uid)
+          .set({
+        'pesoInicial': peso,
+        'fechaPesoInicial': fechaActual,
+      }, SetOptions(merge: true));
+      setState(() {
+        pesoInicial = peso;
+        fechaPesoInicial = fechaActual;
+        _editandoPesoInicial = false;
+      });
+    }
+  }
+
+  Future<void> _guardarPesoActual(double pesoActual) async {
+    User? user = _auth.currentUser;
+    if (user != null && pesoInicial != null) {
+      DateTime fechaActual = DateTime.now();
+      double porcentaje = ((pesoInicial! - pesoActual) / pesoInicial!) * 100;
+
+      // Guardar el peso actual y la fecha en la subcolección historialPesos
+      await FirebaseFirestore.instance
+          .collection('usuarios')
+          .doc(user.uid)
+          .collection('historialPesos')
+          .add({
+        'pesoActual': pesoActual,
+        'fecha': fechaActual,
+        'porcentajePerdido': porcentaje,
+      });
+
+      setState(() {
+        _resultado =
+            'Has perdido un ${porcentaje.toStringAsFixed(2)}% de tu peso inicial.';
+      });
+    }
+  }
+
+  void _calcularPorcentaje() {
+    double? pesoActual = double.tryParse(_pesoActualController.text);
+
+    if (pesoInicial != null && pesoActual != null && pesoInicial! > 0) {
+      _guardarPesoActual(pesoActual);
+    } else {
+      setState(() {
+        _resultado = 'Por favor, ingresa un peso válido.';
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -11,35 +105,118 @@ class ResultadoPeso  extends StatelessWidget {
           icon: Icon(Icons.arrow_back, color: Colors.white),
           onPressed: () => Navigator.of(context).pop(),
         ),
-        title: Text('Porcentaje de peso', style: TextStyle(color: Colors.white)),
+        title:
+            Text('Seguimiento de Peso', style: TextStyle(color: Colors.white)),
         elevation: 0,
       ),
       body: Container(
-        color: Color.fromARGB(255, 255, 255, 255),
+        color: Colors.white,
         child: Padding(
           padding: EdgeInsets.all(16.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(height: 20),
-                Text(overflow: TextOverflow.ellipsis, 'Segun el peso inicial versus el peso actualmente logrado el porcentaje de grasa perdida es:'),
-             
+              _editandoPesoInicial
+                  ? Column(
+                      children: [
+                        Text(
+                          'Actualiza tu peso inicial:',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        SizedBox(height: 20),
+                        TextFormField(
+                          controller: _pesoInicialController,
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                            labelText: 'Peso Inicial (kg)',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        ElevatedButton(
+                          onPressed: () {
+                            double? nuevoPesoInicial =
+                                double.tryParse(_pesoInicialController.text);
+                            if (nuevoPesoInicial != null) {
+                              _guardarPesoInicial(nuevoPesoInicial);
+                            }
+                          },
+                          child: Text('Guardar Peso Inicial'),
+                          style: ElevatedButton.styleFrom(
+                            minimumSize: Size(double.infinity, 50),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            setState(() {
+                              _editandoPesoInicial = false;
+                            });
+                          },
+                          child: Text("Cancelar"),
+                        ),
+                      ],
+                    )
+                  : Column(
+                      children: [
+                        Text(
+                          'Tu peso inicial registrado es: ${pesoInicial != null ? pesoInicial!.toStringAsFixed(1) : 'No registrado'} kg\n'
+                          'Fecha de registro: ${fechaPesoInicial != null ? fechaPesoInicial!.toLocal().toString().split(' ')[0] : 'No registrada'}',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            setState(() {
+                              _editandoPesoInicial = true;
+                            });
+                          },
+                          child: Text("Actualizar Peso Inicial"),
+                        ),
+                        SizedBox(height: 20),
+                        TextFormField(
+                          controller: _pesoActualController,
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                            labelText: 'Peso Actual (kg)',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                        SizedBox(height: 24),
+                        ElevatedButton(
+                          onPressed: _calcularPorcentaje,
+                          child: Text('Calcular Progreso'),
+                          style: ElevatedButton.styleFrom(
+                            minimumSize: Size(double.infinity, 50),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 24),
+                        Text(
+                          _resultado,
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+              SizedBox(height: 24),
               ElevatedButton(
-                child: Text('Regresar', style: TextStyle(color: Color(0xFFFFA07A))),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('Regresar',
+                    style: TextStyle(color: Color(0xFFFFA07A))),
                 style: ElevatedButton.styleFrom(
                   minimumSize: Size(double.infinity, 50),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(8),
-                    
                   ),
                 ),
-                onPressed: () {
-                  // Handle registration logic
-                },
               ),
-                SizedBox(height: 24),
-              
-              
-            
             ],
           ),
         ),
@@ -47,4 +224,3 @@ class ResultadoPeso  extends StatelessWidget {
     );
   }
 }
-
